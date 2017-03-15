b63d673b8d8602fdde19837486fe52cadd13c0fe
Milos Zivanovic <tihiway@hotmail.com>
2017-03-15 20:30:54 +0100

    Fixed errors

--- WebStore.Data/UserRepository.cs
+++ WebStore.Data/UserRepository.cs
@@ -41,7 +41,7 @@ namespace WebStore.Data
             adp.Fill(ds);
             var myData = ds.Tables[0].AsEnumerable().Select(r => new User
             {
-<<<<<<< HEAD
+
                 UserName = r.Field<string>("UserName"),
                 Password = r.Field<string>("Password"),
                 Email = r.Field<string>("Email"),
@@ -49,17 +49,11 @@ namespace WebStore.Data
             });
            return myData.ToList();
             
-=======
-                new User
-                {
-                    UserName= "pera",
-                    Password="123",
-                    Email = "pera@peran.com",
-                    Credit = 1000
-                }
+
               
-            };
->>>>>>> afd43be49a46b45688c529871ba11f37fa537811
+              
+            
+
         }
     }
 }
