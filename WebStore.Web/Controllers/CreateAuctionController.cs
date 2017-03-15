1167a266a5729182a4684bb19cea0160fbdf4445
Milos Zivanovic <tihiway@hotmail.com>
2017-03-15 13:28:04 +0100

    Create auction page

--- WebStore.Web/Controllers/CreateAuctionController.cs
+++ WebStore.Web/Controllers/CreateAuctionController.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebStore.Services.Contracts.Dto;
+using WebStore.Services.Contracts.ServiceInterface;
 
 namespace WebStore.Web.Controllers
 {
     public class CreateAuctionController : Controller
     {
+        private readonly IUserService _userService;
+        public CreateAuctionController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            AuctionDto auction = new AuctionDto();
+            auction.Name = Request["name"];
+            auction.Price = Convert.ToDouble(Request["price"]);
+            auction.Seller = "Pera";
+            
+            // _userService.CreateAuction(auction);
+            return RedirectToAction("Index", "Home", null);
+        }
+
         // GET: CreateAuction
         public ActionResult Index()
         {
